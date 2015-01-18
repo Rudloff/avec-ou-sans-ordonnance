78b5893b5552e12bafbd87d81115a3a2153c0fd9
Pierre Rudloff <contact@rudloff.pro>
2015-01-18 04:23:02 +0100

    PHPCS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,23 @@
 /*jslint node: true */
 module.exports = function (grunt) {
     'use strict';
-    grunt.initConfig({
-        cssmin: {
-            combine: {
-                files: {
-                    'dist/main.css': ['css/style.css']
+    grunt.initConfig(
+        {
+            cssmin: {
+                combine: {
+                    files: {
+                        'dist/main.css': ['css/style.css']
+                    }
+                }
+            },
+            watch: {
+                styles: {
+                    files: ['css/*.css'],
+                    tasks: ['cssmin']
                 }
             }
-        },
-        watch: {
-            styles: {
-                files: ['css/*.css'],
-                tasks: ['cssmin']
-            }
         }
-    });
+    );
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
