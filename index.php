8dec106157bb86d061b2e1822c7e2780108d2da6
Pierre Rudloff <contact@rudloff.pro>
2015-04-10 20:54:40 +0200

    Composer for Smarty

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
+require_once 'vendor/autoload.php';
 require_once 'config.php';
-require_once 'smarty/Smarty.class.php';
 $smarty = new Smarty();
 $smarty->setTemplateDir('templates/');
 $pdo = new PDO('mysql:dbname='.DBNAME.';host=localhost', DBUSER, DBPASS);
